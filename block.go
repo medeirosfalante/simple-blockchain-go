182d0837fa7ad173218cde5457103fa16ee0d92d
Rafael Medeiros <98@99.exchange>
2018-07-16 10:16:31 -0300

    create a func setHash

--- block.go
+++ block.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+)
+
 // Block structure
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+}
+
+// SetHash is a func setter a hash in blockch
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	header := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(header)
+	b.Hash = hash[:]
 }
