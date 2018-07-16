72e38bf64d10878d48f71512ab08fe1588bd1f80
Rafael Medeiros <98@99.exchange>
2018-07-16 10:36:16 -0300

    add a new block in blockchain

---
+++ blokchain.go
@@ -0,0 +1,13 @@
+package main
+
+//Blockchain is a struct for blockchain database
+type Blockchain struct {
+	blocks []*Block
+}
+
+//AddBlock is a func add a new block in blockchain
+func (b *Blockchain) AddBlock(data string) {
+	prevBlock := b.blocks[len(b.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	b.blocks = append(b.blocks, newBlock)
+}
