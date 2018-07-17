d20ef6e21c427bfd438e2dc8f0a038061110a250
Rafael Medeiros <98@99.exchange>
2018-07-17 17:42:38 -0300

    Deserialize

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -21,4 +24,27 @@ func NewBlock(data string, PrevBlockHash []byte) (block *Block) {
 	block.Hash = hash[:]
 	block.Nonce = nonce
 	return
+}
+
+// Serialize is a func serializa
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result.Bytes()
+}
+
+// DeserializeBlock is a func deserialize a data
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
 }
