32cec009cc2a3f98705947b48d1fbe94db6dc257
Rafael Medeiros <98@99.exchange>
2018-07-16 23:47:51 -0300

    simple test in main

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to RAFAEL")
+	bc.AddBlock("Send 2 more MARC")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("tutorial Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("tutorial Data: %s\n", block.Data)
+		fmt.Printf(" tutorial Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
