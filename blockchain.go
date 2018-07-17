5e2d8d7e088b202b5aba2112bfdd36d496e03b0e
Rafael Medeiros <98@99.exchange>
2018-07-16 23:46:06 -0300

    fix file name

--- blockchain.go
+++ blockchain.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 //Blockchain is a struct for blockchain database
 type Blockchain struct {
 	blocks []*Block
@@ -20,4 +24,8 @@ func NewGenesisBlock() *Block {
 // NewBlockchain is a func create a new Blockchain
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func Test() {
+	fmt.Println("olá")
 }
