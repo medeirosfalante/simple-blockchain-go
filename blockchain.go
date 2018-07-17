54712f71c03eb76092fab017b07dd3035659d103
Rafael Medeiros <98@99.exchange>
2018-07-17 18:55:02 -0300

    add a database in blockchain

--- blockchain.go
+++ blockchain.go
@@ -1,15 +1,32 @@
 package main
 
+import "github.com/boltdb/bolt"
+
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+
 //Blockchain is a struct for blockchain database
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 //AddBlock is a func add a new block in blockchain
-func (b *Blockchain) AddBlock(data string) {
-	prevBlock := b.blocks[len(b.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	b.blocks = append(b.blocks, newBlock)
+func (bc *Blockchain) AddBlock(data string) {
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash := b.Get([]byte("l"))
+		return nil
+	})
+	newBlock := NewBlock(data, lastHash)
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		bc.tip = newBlock.Hash
+		return nil
+	})
 }
 
 // NewGenesisBlock is a func Create a Genesis block
@@ -19,5 +36,23 @@ func NewGenesisBlock() *Block {
 
 // NewBlockchain is a func create a new Blockchain
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+		return nil
+	})
+	bc := Blockchain{tip, db}
+	return &bc
 }
