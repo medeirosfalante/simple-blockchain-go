ac77ae83680188024fe6206a381d3837cf4d8943
Rafael Medeiros <98@99.exchange>
2018-07-17 23:20:17 -0300

    change structure

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	bc := NewBlockchain()
+	defer bc.db.Close()
 
-	bc.AddBlock("Send 1 BTC to RAFAEL")
-	bc.AddBlock("Send 2 more MARC")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("tutorial Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("tutorial Data: %s\n", block.Data)
-		fmt.Printf(" tutorial Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
