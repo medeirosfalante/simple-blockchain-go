0307f9548816d7a31e72c64941dd4e3013f66d05
Rafael Medeiros <98@99.exchange>
2018-07-17 23:11:22 -0300

    cli and config folter

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,11 @@
-package main
+package blockchain
 
-import "github.com/boltdb/bolt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/boltdb/bolt"
+)
 
 const dbFile = "blockchain.db"
 const blocksBucket = "blocks"
@@ -11,22 +16,57 @@ type Blockchain struct {
 	db  *bolt.DB
 }
 
+//BlockchainIterator is a struct for iterator for blockchain
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
+//Iterator is a func interation in blockchain
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+	return bci
+}
+
+// Next a interator block
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	i.currentHash = block.PrevBlockHash
+	return block
+}
+
 //AddBlock is a func add a new block in blockchain
 func (bc *Blockchain) AddBlock(data string) {
 	var lastHash []byte
-
+	fmt.Printf("%s", data)
 	err := bc.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
-		lastHash := b.Get([]byte("l"))
+		lastHash = b.Get([]byte("l"))
 		return nil
 	})
 	newBlock := NewBlock(data, lastHash)
 	err = bc.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
 		bc.tip = newBlock.Hash
 		return nil
 	})
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 // NewGenesisBlock is a func Create a Genesis block
@@ -38,12 +78,18 @@ func NewGenesisBlock() *Block {
 func NewBlockchain() *Blockchain {
 	var tip []byte
 	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	err = db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		if b == nil {
 			genesis := NewGenesisBlock()
 			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				log.Panic(err)
+			}
 			err = b.Put(genesis.Hash, genesis.Serialize())
 			err = b.Put([]byte("l"), genesis.Hash)
 			tip = genesis.Hash
