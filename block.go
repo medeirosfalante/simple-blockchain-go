62417be2b813181450179dfcec4608c286093d5c
Rafael Medeiros <98@99.exchange>
2018-07-26 09:37:07 -0300

    adapter merkle in hash Block transactions

--- block.go
+++ block.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"crypto/sha256"
 	"encoding/gob"
 	"log"
 	"time"
@@ -57,12 +56,11 @@ func NewGenesisBlock(coinbase *Transaction) *Block {
 
 // HashTransactions return a hash transaction
 func (b *Block) HashTransactions() []byte {
-	var txHashes [][]byte
-	var txHash [32]byte
+	var transactions [][]byte
 
 	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.ID)
+		transactions = append(transactions, tx.Serialize())
 	}
-	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
-	return txHash[:]
+	mTree := NewMerkleTree(transactions)
+	return mTree.RootNode.Data
 }
